488f348f0c1883412bf93934046563eb1940c52e
bgcode <violetcode@icloud.com>
2024-10-16 18:08:14 +0800

    frist update

---
+++ popup.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: bgcode
+ * @Date: 2024-10-16 18:02:54
+ * @LastEditTime: 2024-10-16 18:03:01
+ * @LastEditors: bgcode
+ * @Description: 描述
+ * @FilePath: /myplugin/popup.js
+ * 本项目采用GPL 许可证，欢迎任何人使用、修改和分发。
+ */
+document.getElementById('myButton').addEventListener('click', () => {
+    alert('Button was clicked!');
+});
